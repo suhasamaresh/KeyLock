9fd9764adfa3296ccb109d426bf6ba6e364cfd30
Suhas A <suhasamaresh@gmail.com>
2025-08-12 22:46:52 +0530

    fix: changed port binding to 0.0.0.0

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,24 @@
-use axum::{routing::{get,post}, Router};
-use tower_http::cors::CorsLayer;
 use axum::http::Method;
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use tower_http::cors::CorsLayer;
 
-mod routes;
-mod db;
-mod crypto;
 mod config;
+mod crypto;
+mod db;
 mod error;
+mod routes;
 mod utils;
 
-
-use crate::routes::{share::share_secret, retrieve::get_secret};
-use crate::crypto::CryptoService;
 use crate::config::establish_connection;
-use crate::routes::AppState;
-use std::sync::{Arc, Mutex};
-use tokio_cron_scheduler::{JobScheduler, Job};
+use crate::crypto::CryptoService;
 use crate::db::cleanup::cleanup_expired_records;
+use crate::routes::AppState;
+use crate::routes::{retrieve::get_secret, share::share_secret};
+use std::sync::{Arc, Mutex};
+use tokio_cron_scheduler::{Job, JobScheduler};
 
 #[tokio::main]
 async fn main() {
@@ -34,7 +36,10 @@ async fn main() {
     let cors = CorsLayer::new()
         .allow_origin(tower_http::cors::Any)
         .allow_methods([Method::GET, Method::POST])
-        .allow_headers(vec![axum::http::header::CONTENT_TYPE, axum::http::header::ACCEPT]);
+        .allow_headers(vec![
+            axum::http::header::CONTENT_TYPE,
+            axum::http::header::ACCEPT,
+        ]);
 
     let app = Router::new()
         .route("/api/share", post(share_secret))
@@ -42,20 +47,29 @@ async fn main() {
         .with_state(state)
         .layer(cors);
 
-    
-    sched.add(
-        Job::new_async("0 0 0 * * 0", {
-            let db_pool = db_pool.clone();
-            move |_uuid, _| {
+    sched
+        .add(
+            Job::new_async("0 0 0 * * 0", {
                 let db_pool = db_pool.clone();
-                Box::pin(async move{
-                    println!("Hi deleting expired records");
-                    cleanup_expired_records(db_pool).await;
-                })
-            }
-        }).unwrap()
-    ).await.unwrap();
+                move |_uuid, _| {
+                    let db_pool = db_pool.clone();
+                    Box::pin(async move {
+                        println!("Hi deleting expired records");
+                        cleanup_expired_records(db_pool).await;
+                    })
+                }
+            })
+            .unwrap(),
+        )
+        .await
+        .unwrap();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001").await.unwrap();
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "10000".to_string())
+        .parse::<u16>()
+        .expect("PORT must be a valid number");
+
+    let address = format!("0.0.0.0:{}", port);
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
